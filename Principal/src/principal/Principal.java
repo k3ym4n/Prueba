6e9ba312597c3a666074941a34d3f2bc8f95cef2
k3ym4n <lizardte@gmail.com>
2015-02-03 19:31:39 +0100

    V0.1

---
+++ Principal/src/principal/Principal.java
@@ -0,0 +1,12 @@
+package principal;
+import java.util.Scanner;
+public class Principal {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Hola github and git");
+
+	}
+
+}
